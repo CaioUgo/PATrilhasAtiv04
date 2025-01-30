84494483b935d6a2f2bca739f48e261c87bc2a7f
Caio <168545443+CaioUgo@users.noreply.github.com>
2025-01-30 10:47:40 -0300

    Atividade5-D.cs

---
+++ Atividade5-D.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace PrimeiraAtividade
+{
+	class Program
+	{
+		public static void Main()
+		{	
+
+			Console.Write("Digite uma palavra: ");
+			string palavra = Console.ReadLine();
+			
+			string tamanho = (palavra.Length == 0) ? "\n\rA string está vazia.": "\n\rA string não está vazia.";
+			                   
+			Console.WriteLine(tamanho);
+		
+			Console.WriteLine("\n\rAperte alguma tecla para fechar...");
+			Console.ReadKey(true);
+		}
+	}
+}
